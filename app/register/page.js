e276571df971a39c7a5f27d173a229a8ca2e3d15
Marc <mgranier@comcast.net>
2023-12-06 16:59:40 -0500

    Created register form

--- app/register/page.js
+++ app/register/page.js
@@ -11,6 +11,7 @@ export default function Register() {
 		e.preventDefault();
 		try {
 			setLoading(true);
+			console.log(name, email, password);
 		} catch (err) {
 			console.log(err);
 			setLoading(false);
@@ -22,6 +23,37 @@ export default function Register() {
 				<div className='row d-flex justify-content-center align-items-center vh-100'>
 					<div className='col-lg-5 shadow bg-light p-5'>
 						<h2 className='mb-4 text-center'>Register</h2>
+
+						<form onSubmit={handleSubmit}>
+							<input
+								type='text'
+								value={name}
+								onChange={(e) => setName(e.target.value)}
+								className='form-control mb-4'
+								placeholder='Enter your name'
+							/>
+							<input
+								type='email'
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+								className='form-control mb-4'
+								placeholder='Enter your email'
+							/>
+							<input
+								type='password'
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+								className='form-control mb-4'
+								placeholder='Enter your password'
+							/>
+
+							<button
+								className='btn btn-primary btn-raised'
+								disabled={loading || !name || !email || !password}
+							>
+								{loading ? 'Please wait..' : 'Submit'}
+							</button>
+						</form>
 					</div>
 				</div>
 			</div>
