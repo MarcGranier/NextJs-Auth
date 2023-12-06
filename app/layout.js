a72b6f788d6f81bdf011efcc8d0df5dd0a918c09
Marc <mgranier@comcast.net>
2023-12-06 14:42:15 -0500

    Initial commit - Bootstrap Installed

---
+++ app/layout.js
@@ -0,0 +1,18 @@
+import './globals.css';
+import 'bootstrap-material-design/dist/css/bootstrap-material-design.min.css';
+
+export const metadata = {
+	title: 'Nextcom',
+	description: 'Ecommerce app using NextJs Full Stack',
+};
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang='en'>
+			<body>
+				//
+				{children}
+			</body>
+		</html>
+	);
+}
