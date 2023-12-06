a1ea1c0379427b6dbaf65aaa3e7768857d3faaae
Marc <mgranier@comcast.net>
2023-12-06 15:53:47 -0500

    Create User Model

---
+++ models/user.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, 'Name is required'],
+			trim: true,
+		},
+		email: {
+			type: String,
+			required: [true, 'Email is required'],
+			index: true,
+			lowercase: true,
+			unique: true,
+			trim: true,
+		},
+		password: String,
+		role: {
+			type: String,
+			default: 'user',
+		},
+		image: String,
+		resetCode: {
+			data: String,
+			expiresAt: {
+				type: Date,
+				default: () => new Date(Date, now() + 10 * 60 * 1000),
+			},
+		},
+	},
+	{ timestamps: true }
+);
+
+export default mongoose.models.User || mongoose.model('User', userSchema);
