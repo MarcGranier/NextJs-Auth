92fcffabf1f95fa22662cb6f18d37454582d0049
Marc <mgranier@comcast.net>
2023-12-07 14:10:58 -0500

    Logged in user info and Logout

--- components/TopNav.js
+++ components/TopNav.js
@@ -4,21 +4,36 @@ import { useSession, signOut } from 'next-auth/react';
 
 export default function TopNav() {
 	const { data, status, loading } = useSession();
-	console.log({ data, status });
+	// console.log({ data, status });
 
 	return (
 		<nav className='nav shadow p-2 justify-content-between mb-3'>
 			<Link href='/' className='nav-link'>
 				🛒NEXTCOM
 			</Link>
-			<div className='d-flex'>
-				<Link href='/login' className='nav-link'>
-					Login
-				</Link>
-				<Link href='/register' className='nav-link'>
-					Register
-				</Link>
-			</div>
+
+			{status === 'authenticated' ? (
+				<>
+					<Link href='/dashboard/user' className='nav-link'>
+						{data?.user?.name}
+					</Link>
+					<a
+						className='nav-link pointer'
+						onClick={() => signOut({ callbackUrl: '/login' })}
+					>
+						Logout
+					</a>
+				</>
+			) : (
+				<div className='d-flex'>
+					<Link href='/login' className='nav-link'>
+						Login
+					</Link>
+					<Link href='/register' className='nav-link'>
+						Register
+					</Link>
+				</div>
+			)}
 		</nav>
 	);
 }
